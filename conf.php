7ae586713b48869fde1477c4a3a4d8ba7d62a814
nilukilu <79317759+nilukilu@users.noreply.github.com>
2021-06-20 22:19:56 +0300

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+
+$server_user_name = "siimseppi";
+$server_password = "rhdmVIdF08";
+$server_host = "localhost";
+$database = "siimseppi";
---
+++ conf2.php
@@ -0,0 +1,6 @@
+<?php
+	
+	$server_user_name = "jaanussaago";
+	$server_password = "LXPU987aed";
+	$server_host = "localhost";
+	$database = "jaanussaago";
